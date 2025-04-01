424376eb5649d99e341a7a4f5ba3fd1945e71d0f
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-04-01 17:54:46 -0600

    Mis cosas

--- L02P02_2022MS651_2022ZR650/Models/libreriaDbContext.cs
+++ L02P02_2022MS651_2022ZR650/Models/libreriaDbContext.cs
@@ -5,7 +5,7 @@ namespace L02P02_2022MS651_2022ZR650.Models
     public class libreriaDbContext : DbContext
     {
 
-        libreriaDbContext(DbContextOptions options) : base(options)
+        public libreriaDbContext(DbContextOptions<libreriaDbContext> options) : base(options)
         {
 
         }
