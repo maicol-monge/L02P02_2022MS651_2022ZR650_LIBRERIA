6aa4e753dc8e212d1555e252189b3ec0f3d2fc63
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-04-01 17:19:19 -0600

    Add project files.

---
+++ L02P02_2022MS651_2022ZR650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022MS651_2022ZR650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ L02P02_2022MS651_2022ZR650/Models/libreriaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022MS651_2022ZR650.Models
+{
+    public class libreriaDbContext : DbContext
+    {
+
+        libreriaDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
